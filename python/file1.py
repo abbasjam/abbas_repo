3e0b890983e6ffa1b3d93abb702d0f7090311f85
ERP <ERP@GIS-ERP-SYS18>
2019-12-24 17:40:47 +0530

    add print command

--- python/file1.py
+++ python/file1.py
@@ -1,10 +1,8 @@
 f=open("classtest.py","r")
 print(f.read())
-
+print("Tested")
 f=open("classtest.py","a")
 f.write("test added using new write option")
 
 f=open("classtest.py","r")
 print (f.read())
-
-
